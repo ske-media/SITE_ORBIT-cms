91456e44baceedde516b8775967467174ea98b5e
Steven C. K. ELDRING <seldring@SKE.local>
2025-03-13 00:03:36 +0100

    Configure jwtSecret in plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,9 @@
-module.exports = ({ env }) => {
-  console.log('JWT_SECRET =>', env('jwtSecret'));
-  return {
-    'users-permissions': {
-      config: {
-        jwtSecret: env('jwtSecret', 'fallback-secret'),
-      },
+const crypto = require('crypto');
+
+module.exports = ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET') || crypto.randomBytes(16).toString('base64'),
     },
-  };
-};
+  },
+});
